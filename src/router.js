a5741ba3ce88aa0770a0b43b3f3d58deaa781ece
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-07-01 17:03:53 +0200

    add some , and ;

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebhHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import ProjectsPage from "./pages/ProjectsPage.vue";
 
-const router = createRouter  ({
-    history: createWebHistory();
+const router = createRouter ({
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
